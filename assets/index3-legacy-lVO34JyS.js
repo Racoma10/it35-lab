53cb045c762712cb8b224d36e25c983bf627cc24
Racoma10 <Daryldada697@gmail.com>
2025-03-10 08:38:07 +0800

    Updates

--- assets/index3-legacy-lVO34JyS.js
+++ assets/index3-legacy-lVO34JyS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MlYR18XP.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-C-7a4B4l.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-DQqz84sS.js
+++ assets/keyboard2-legacy-DQqz84sS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MlYR18XP.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-C-7a4B4l.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
